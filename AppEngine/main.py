7ac449649d3f550851f1cd190bbf6a771a6a488e
afmirza2 <afmirza2@illinois.edu>
2018-02-24 17:45:28 -0600

    main

--- AppEngine/main.py
+++ AppEngine/main.py
@@ -3,15 +3,45 @@ from google.appengine.ext import ndb
 
 import webapp2
 
-def get(self):
-    bucket_name = os.environ.get(
-        'openfashion-196206.appspot.com', app_identity.get_default_gcs_bucket_name())
+class MainPage(webapp2.RequestHandler):
+    def get(self):
+        self.response.headers['Content-Type'] = 'text/plain'
+        self.response.write('Hello, World!')
 
-    self.response.headers['Content-Type'] = 'text/plain'
-    self.response.write(
-        'Demo GCS Application running from Version: {}\n'.format(
-            os.environ['CURRENT_VERSION_ID']))
-    self.response.write('Using bucket name: {}\n\n'.format(bucket_name))
+
+app = webapp2.WSGIApplication([
+    ('/', MainPage),
+], debug=True)
+
+# def get(self):
+#     bucket_name = os.environ.get(
+#         'openfashion-196206.appspot.com', app_identity.get_default_gcs_bucket_name())
+
+#     self.response.headers['Content-Type'] = 'text/plain'
+#     self.response.write(
+#         'Demo GCS Application running from Version: {}\n'.format(
+#             os.environ['CURRENT_VERSION_ID']))
+#     self.response.write('Using bucket name: {}\n\n'.format(bucket_name))
+
+# def create_file(self, filename):
+#     """Create a file."""
+
+#     self.response.write('Creating file {}\n'.format(filename))
+
+#     # The retry_params specified in the open call will override the default
+#     # retry params for this particular file handle.
+#     write_retry_params = cloudstorage.RetryParams(backoff_factor=1.1)
+#     with cloudstorage.open(
+#         filename, 'w', content_type='text/plain', options={
+#             'x-goog-meta-foo': 'foo', 'x-goog-meta-bar': 'bar'},
+#             retry_params=write_retry_params) as cloudstorage_file:
+#                 cloudstorage_file.write('abcde\n')
+#                 cloudstorage_file.write('f'*1024*4 + '\n')
+#     self.tmp_filenames_to_clean_up.append(filename)
+
+
+# if __name__ == "__main__": 
+# 	create_file(self, "test.txt")
 
 # def list_bucket(self, bucket):
 #   """Create several files and paginate through them.
